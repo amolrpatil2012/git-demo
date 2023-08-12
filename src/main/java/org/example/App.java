c9e025486b49d27f2796409dea298a5509063bb6
amolpatil <amolrpatil2012@gmail.com>
2023-08-12 20:13:00 +0530

    Created Local Repository

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,17 @@
+package org.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void helloWorld ( String name )
+    {
+        System.out.println( " Hello " + name );
+    }
+    public static void main( String[] args )
+    {
+        helloWorld("Rohit");
+    }
+}
